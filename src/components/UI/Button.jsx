281ef0f228bc43414bc452f85913eceb92d2b660
Kanykei <kanisatybaldyeva@gmail.com>
2023-10-06 14:18:49 +0600

    added Button componnet

---
+++ src/components/UI/Button.jsx
@@ -0,0 +1,65 @@
+import { Button as UiButton, styled } from '@mui/material'
+import React from 'react'
+
+export const Button = ({
+   children,
+   variant = 'outlined',
+   onClick,
+   ...rest
+}) => {
+   return (
+      <StyledButton variant={variant} onClick={onClick} {...rest}>
+         {children}
+      </StyledButton>
+   )
+}
+
+const StyledVariants = {
+   primary: {
+      backgroundColor: '#8639b5',
+      color: 'white',
+      fontSize: '16px',
+      fontWeight: 500,
+      border: 'none',
+      ':hover': { backgroundColor: '#612386' },
+      ':active': { backgroundColor: '#793686' },
+      ':disabled': { backgroundColor: 'rgba(28, 27, 31, 0.12)' },
+   },
+   outlined: {
+      backgroundColor: '#fafafa',
+      color: '#8D949E',
+      fontSize: '14px',
+      fontWeight: 500,
+      border: '1px solid #8D949E',
+      ':hover': { backgroundColor: '#612386', color: 'white' },
+      ':active': { backgroundColor: '#AB62D8', color: 'white' },
+      ':disabled': {
+         backgroundColor: 'white',
+         border: '1px solid rgba(28, 27, 31, 0.12)',
+         color: 'rgba(28, 27, 31, 0.12)',
+      },
+   },
+   contained: {
+      backgroundColor: '#FA2B56',
+      color: 'white',
+      fontSize: '16px',
+      fontWeight: 600,
+      border: 'none',
+      ':hover': { backgroundColor: '#DD0B37' },
+      ':active': { backgroundColor: '#ED6380' },
+      ':disabled': { backgroundColor: 'rgba(28, 27, 31, 0.12)' },
+   },
+}
+
+const getVariants = (props) => {
+   const currentVariant = StyledVariants[props.variant]
+   return currentVariant
+}
+
+const StyledButton = styled(UiButton)`
+   padding: 16px 18px 16px 18px;
+   border-radius: 6px;
+   outline: none;
+   cursor: pointer;
+   ${getVariants}
+`
