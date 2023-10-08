0d817bec9a7629850adc004fe1e5729e117f7202
Kanykei <kanisatybaldyeva@gmail.com>
2023-10-08 11:12:09 +0600

    added empty components

---
+++ src/components/EmptyComponent.jsx
@@ -0,0 +1,46 @@
+import {
+   Typography,
+   Button,
+   CardMedia,
+   CardContent,
+   styled,
+   Box,
+} from '@mui/material'
+
+import Empty from '../assets/images/Empty.png'
+
+export const EmptyComponent = () => {
+   return (
+      <Container>
+         <Typography variant="h6">Список желаний</Typography>
+         <EmptyContent>
+            <CardMedia
+               image={Empty}
+               title="empty-image"
+               component="img"
+               alt="empty-image"
+            />
+
+            <CardContent>Вы пока не добавили желание!</CardContent>
+            <Button variant="contained">+ Добавить желание</Button>
+            {/* // azyryncha mui button koshup turdum kiyin Button componneta merge
+         bolgondo oshonu chakyryp koiobuz// */}
+         </EmptyContent>
+      </Container>
+   )
+}
+
+const EmptyContent = styled(Box)({
+   width: '600px',
+   margin: '0 auto',
+   height: '100vh',
+   display: 'flex',
+   flexDirection: 'column',
+   gap: '33px',
+   alignItems: 'center',
+})
+
+const Container = styled('div')({
+   background: '#F7F8FA',
+   padding: '20px',
+})
