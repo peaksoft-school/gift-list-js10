06471fdcc64cdfaa0718f7df2e19d722cc6ed0ef
Kamchybek <kamchybek@users-MacBook-Pro.local>
2023-10-10 16:30:12 +0600

    clear app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,3 @@
-// import { SearchSelect } from './components/UI/SearchSelect'
-
 export function App() {
-   // const { values, handleChange, handleReset } = useFormik({
-   //    initialValues: {
-   //       state: '',
-   //       category: '',
-   //       subCategory: '',
-   //       country: '',
-   //       search: '',
-   //    },
-   // }) // formik тин зависимостун кочуруш керек
-   return (
-      <div className="App">
-         {/* <SearchSelect
-            values={values}
-            handleChange={handleChange}
-            handleReset={handleReset}
-            variant="select" //вариантты бербесек обычный инпут болот
-         /> */}
-      </div>
-   )
+   return <div className="App">App</div>
 }
