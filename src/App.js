da5a4894f71c6a14685ef2da116caa53d1b89ca3
Mamatova Nuraiym <mnurajym9@gmail.com>
2023-10-14 17:39:58 +0600

    added secondary list variant

--- src/App.js
+++ src/App.js
@@ -1,25 +1,3 @@
-import { Card } from './components/UI/Card'
-
 export function App() {
-   return (
-      <div
-         className="App"
-         style={{
-            display: 'flex',
-            width: '100%',
-            height: '100%',
-            gap: '20px',
-            flexDirection: 'column',
-         }}
-      >
-         <Card />
-         <Card list />
-         <Card variant="secondary" />
-         <Card variant="secondary" list />
-         <Card variant="tertiary" />
-         <Card variant="quaternary" />
-         <Card variant="withStatusBottom" />
-         <Card variant="withStatusTop" />
-      </div>
-   )
+   return <div className="App">App</div>
 }
