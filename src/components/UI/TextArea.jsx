3033020f90bedf455270f5f8c94e16873795da39
Kasym Ulanbekov <ulanbek.kasym@gmail.com>
2023-10-16 16:44:41 +0600

    finished

--- src/components/UI/TextArea.jsx
+++ src/components/UI/TextArea.jsx
@@ -1,15 +1,29 @@
-import { TextField as MuiTextField, styled } from '@mui/material'
+import { FormControl, TextField as MuiTextField, styled } from '@mui/material'
 import React from 'react'
 
 export const TextArea = ({ value, onChange, labelText }) => {
    return (
-      <StyledMuiTextField value={value} onChange={onChange} label={labelText} />
+      <StyledMuiFormControl>
+         <StyledMuiTextField
+            value={value}
+            onChange={onChange}
+            label={labelText}
+         />
+      </StyledMuiFormControl>
    )
 }
 
 const StyledMuiTextField = styled(MuiTextField)`
    width: 50.5rem;
    height: 6.938rem;
-   border-radius: 60px;
-   border: solid;
+`
+
+const StyledMuiFormControl = styled(FormControl)`
+   .css-wxa1hi-MuiInputBase-root-MuiOutlinedInput-root {
+      display: inline-block;
+      padding: 8px 18px;
+      width: 100%;
+      height: 100%;
+      border-radius: 6px;
+   }
 `
