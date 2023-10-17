39a2d0add2d00dcd45503917a7b2184f4f6c798e
Kanykei <kanisatybaldyeva@gmail.com>
2023-10-17 13:42:13 +0600

    changed style and props

--- src/components/UI/SelectComponent.jsx
+++ src/components/UI/SelectComponent.jsx
@@ -8,39 +8,17 @@ import {
 
 import React from 'react'
 
-const selectItems = [
-   {
-      text: 'Электроника',
-      id: '1',
-   },
-   {
-      text: 'Одежда',
-      id: '2',
-   },
-   {
-      text: 'Школа',
-      id: '3',
-   },
-   {
-      text: 'Дом и сад',
-      id: '4',
-   },
-   {
-      text: 'Обувь',
-      id: '5',
-   },
-   {
-      text: 'Транспорт',
-      id: '6',
-   },
-]
-
-export const SelectComponent = ({ onChange, value, label = 'Категория' }) => {
+export const SelectComponent = ({
+   onChange,
+   value,
+   label = 'Категория',
+   props,
+}) => {
    return (
       <StyledFormControl fullWidth>
          <InputLabel>{label}</InputLabel>
          <StyledSelect onChange={onChange} value={value} label={label}>
-            {selectItems.map((i) => {
+            {props.map((i) => {
                return (
                   <SelectContainer key={i.id} value={i.text}>
                      {i.text}
@@ -64,7 +42,7 @@ const SelectContainer = styled(MenuItem)({
    listStyle: 'none',
    alignItems: 'flex-start',
    fontSize: '0.875rem',
-   height: '5.5vh',
+   height: '5vh',
    padding: '9px 16px',
    ':hover': { backgroundColor: ' rgba(112, 46, 153, 0.4)' },
    ':active': { backgroundColor: '#9b6db7' },
