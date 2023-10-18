d9e199b1896ca0a051afdb89975720f7bca69640
Mamatova Nuraiym <mnurajym9@gmail.com>
2023-10-18 22:49:59 +0600

    select sizes added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+import { useState } from 'react'
+import { SizesSelect } from './components/SizesSelect'
+
 export function App() {
-   return <div>App</div>
+   const [selectedSize, setSelectedSize] = useState('')
+   const changeSelectedSize = (e) => setSelectedSize(e.target.value)
+   return (
+      <div style={{ margin: '50px' }}>
+         <SizesSelect
+            selectedSize={selectedSize}
+            handleChange={changeSelectedSize}
+         />
+      </div>
+   )
 }
---
+++ src/components/SizesSelect.jsx
@@ -0,0 +1,70 @@
+import { FormControl, FormLabel, MenuItem, Select, styled } from '@mui/material'
+import React from 'react'
+import { ArrowIcon } from '../assets'
+
+const variants = {
+   shoe: {
+      labelName: 'Размер обуви',
+      placeholder: 'Выберите размер обуви',
+      sizes: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+   },
+   clothing: {
+      labelName: 'Размер одежды',
+      placeholder: 'Выберите размер одежды',
+      sizes: ['XXS', 'XS', 'S', 'M', 'L', 'XXL', 'XL', 'XXXL'],
+   },
+}
+
+export const SizesSelect = ({
+   selectedSize,
+   handleChange,
+   isShoeSizesRender = false,
+}) => {
+   const { labelName, placeholder, sizes } =
+      variants[isShoeSizesRender ? 'shoe' : 'clothing']
+   return (
+      <FormControl fullWidth>
+         <StyledLabel id="demo-simple-select-label">{labelName}</StyledLabel>
+         <Select
+            IconComponent={ArrowIcon}
+            placeholder={placeholder}
+            labelId="demo-simple-select-label"
+            value={selectedSize}
+            onChange={handleChange}
+            displayEmpty
+            renderValue={(selected) =>
+               selected.length === 0 ? (
+                  <StyledPlaceholder>{placeholder}</StyledPlaceholder>
+               ) : (
+                  selected
+               )
+            }
+         >
+            {sizes.map((size) => (
+               <StyledMenuItem value={size}>{size}</StyledMenuItem>
+            ))}
+         </Select>
+      </FormControl>
+   )
+}
+
+const StyledPlaceholder = styled('span')({
+   color: '#8D949E',
+})
+
+const StyledLabel = styled(FormLabel)({
+   paddingBottom: '5px',
+})
+
+const StyledMenuItem = styled(MenuItem)(({ selected }) => ({
+   background: '#FBFBFB',
+   '&:hover': {
+      background: '#8639B5',
+      color: '#fff',
+   },
+   '&:active': {
+      color: '#8639B5',
+      background: '#FBFBFB',
+   },
+   display: selected && 'none',
+}))
