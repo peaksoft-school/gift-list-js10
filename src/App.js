ef6e7fbcaf860c7c9cd0701a9acca4b43de8a3e8
Kanykei <kanisatybaldyeva@gmail.com>
2023-10-19 21:00:32 +0600

    add tabs component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import { Tabs } from './components/UI/Tabs'
+
 export function App() {
-   return <div>App</div>
+   return (
+      <div>
+         <Tabs friends={5} request={3}>
+            Мои друзья
+         </Tabs>
+         <Tabs>Запросы в друзья</Tabs>
+      </div>
+   )
 }
--- src/components/UI/Tabs.jsx
+++ src/components/UI/Tabs.jsx
@@ -1,5 +1,45 @@
+// import { Box, styled } from '@mui/system'
 import React from 'react'
 
-export const Tabs = ({ children }) => {
-   return <div>{children} </div>
+export const Tabs = ({ children, request }) => {
+   return (
+      <div>
+         {children}
+         <span>{request}</span>
+      </div>
+      // <Container>
+      //    <StyledTabs>
+      //       Мои друзья <span>{friends}</span>
+      //    </StyledTabs>
+      //    <StyledTabs>
+      //       Запросы в друзья <span>{request}</span>
+      //    </StyledTabs>
+      // </Container>
+   )
 }
+
+// const Container = styled('div')({
+//    width: '100%',
+//    height: '4vh',
+//    fontSize: '1rem',
+//    fontWeight: 500,
+//    display: 'flex',
+//    border: '1px solid #8D949E',
+//    borderRadius: '6px',
+//    alignItems: 'center',
+//    justifyContent: 'end',
+// })
+
+// const StyledTabs = styled(Box)({
+//    width: '50%',
+//    height: '3vh',
+//    color: '#8D949E',
+//    ':active': {
+//       background: '#8639B5',
+//       color: 'white',
+//    },
+//    ':hover': {
+//       background: '#8639B5',
+//       color: 'white',
+//    },
+// })
