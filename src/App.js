6ed755bb60870e136ec4de87b7a71baebbecc886
Kanykei <kanisatybaldyeva@gmail.com>
2023-10-19 23:37:34 +0600

    added styles for the tabs component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,3 @@
-import { Tabs } from './components/UI/Tabs'
-
 export function App() {
-   return (
-      <div>
-         <Tabs friends={5} request={3}>
-            Мои друзья
-         </Tabs>
-         <Tabs>Запросы в друзья</Tabs>
-      </div>
-   )
+   return <div>App</div>
 }
--- src/components/UI/Tabs.jsx
+++ src/components/UI/Tabs.jsx
@@ -1,45 +1,68 @@
-// import { Box, styled } from '@mui/system'
+import { Box, styled } from '@mui/system'
 import React from 'react'
 
-export const Tabs = ({ children, request }) => {
+export const Tabs = ({ friends, request }) => {
    return (
-      <div>
-         {children}
-         <span>{request}</span>
-      </div>
-      // <Container>
-      //    <StyledTabs>
-      //       Мои друзья <span>{friends}</span>
-      //    </StyledTabs>
-      //    <StyledTabs>
-      //       Запросы в друзья <span>{request}</span>
-      //    </StyledTabs>
-      // </Container>
+      <Container>
+         <StyledTabs>
+            Мои друзья <P>{friends}</P>
+         </StyledTabs>
+
+         <StyledTabs>
+            Запросы в друзья <P>{request}</P>
+         </StyledTabs>
+      </Container>
    )
 }
 
-// const Container = styled('div')({
-//    width: '100%',
-//    height: '4vh',
-//    fontSize: '1rem',
-//    fontWeight: 500,
-//    display: 'flex',
-//    border: '1px solid #8D949E',
-//    borderRadius: '6px',
-//    alignItems: 'center',
-//    justifyContent: 'end',
-// })
+const Container = styled('div')({
+   width: '100%',
+   height: '4vh',
+   fontSize: '1rem',
+   fontWeight: 500,
+   display: 'flex',
+   border: '1px solid #8D949E',
+   borderRadius: '6px',
+   alignItems: 'center',
+   justifyContent: 'center',
+})
 
-// const StyledTabs = styled(Box)({
-//    width: '50%',
-//    height: '3vh',
-//    color: '#8D949E',
-//    ':active': {
-//       background: '#8639B5',
-//       color: 'white',
-//    },
-//    ':hover': {
-//       background: '#8639B5',
-//       color: 'white',
-//    },
-// })
+const StyledTabs = styled(Box)({
+   display: 'flex',
+   justifyContent: 'center',
+   gap: '6px',
+   width: '50%',
+   height: '3.6vh',
+   color: '#8D949E',
+   borderRadius: '6px',
+   alignItems: 'center',
+   padding: '3px',
+   ':active': {
+      background: '#8639B5',
+      color: 'white',
+      '& > p': {
+         background: 'white',
+         color: '#8639B5',
+      },
+   },
+   ':hover': {
+      background: '#8639B5',
+      color: 'white',
+      '& > p': {
+         background: 'white',
+         color: '#8639B5',
+      },
+   },
+})
+
+const P = styled('p')({
+   width: '3%',
+   height: '2.5vh',
+   background: '#595656',
+   borderRadius: '50px',
+   color: 'white',
+   fontSize: '0.875rem',
+   fontWeight: 400,
+   textAlign: 'center',
+   paddingTop: '1px',
+})
