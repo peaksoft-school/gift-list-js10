804c00e5942229ca7884373c91ce88cb55adbfa0
Kanykei <kanisatybaldyeva@gmail.com>
2023-10-20 22:52:13 +0600

    changed tabs component

--- src/components/UI/Tabs.jsx
+++ src/components/UI/Tabs.jsx
@@ -1,68 +1,35 @@
-import { Box, styled } from '@mui/system'
+import { Box, styled } from '@mui/material'
 import React from 'react'
 
-export const Tabs = ({ friends, request }) => {
+export const Tabs = ({ count, isActive, title }) => {
    return (
-      <Container>
-         <StyledTabs>
-            Мои друзья <P>{friends}</P>
-         </StyledTabs>
-
-         <StyledTabs>
-            Запросы в друзья <P>{request}</P>
-         </StyledTabs>
-      </Container>
+      <StyledTabs isActive={isActive}>
+         {title} <P isActive={isActive}>{count}</P>
+      </StyledTabs>
    )
 }
 
-const Container = styled('div')({
-   width: '100%',
-   height: '4vh',
-   fontSize: '1rem',
-   fontWeight: 500,
-   display: 'flex',
-   border: '1px solid #8D949E',
-   borderRadius: '6px',
-   alignItems: 'center',
-   justifyContent: 'center',
-})
-
-const StyledTabs = styled(Box)({
+const StyledTabs = styled(Box)(({ isActive }) => ({
    display: 'flex',
    justifyContent: 'center',
    gap: '6px',
    width: '50%',
    height: '3.6vh',
-   color: '#8D949E',
    borderRadius: '6px',
    alignItems: 'center',
    padding: '3px',
-   ':active': {
-      background: '#8639B5',
-      color: 'white',
-      '& > p': {
-         background: 'white',
-         color: '#8639B5',
-      },
-   },
-   ':hover': {
-      background: '#8639B5',
-      color: 'white',
-      '& > p': {
-         background: 'white',
-         color: '#8639B5',
-      },
-   },
-})
+   background: isActive ? '#8639B5' : 'white',
+   color: isActive ? 'white' : '#8D949E',
+}))
 
-const P = styled('p')({
+const P = styled('span')(({ isActive }) => ({
    width: '3%',
    height: '2.5vh',
-   background: '#595656',
    borderRadius: '50px',
-   color: 'white',
    fontSize: '0.875rem',
    fontWeight: 400,
    textAlign: 'center',
    paddingTop: '1px',
-})
+   background: isActive ? 'white' : '#595656',
+   color: isActive ? '#8639B5' : 'white',
+}))
