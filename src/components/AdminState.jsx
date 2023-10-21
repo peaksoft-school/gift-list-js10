829eed7bc2ff7710bb78ccc6bc13b34476e91403
ari1111i <arietomarkulov2810@gmail.com>
2023-10-22 03:25:14 +0600

    by

--- src/components/AdminState.jsx
+++ src/components/AdminState.jsx
@@ -1,36 +1,82 @@
-import { Button } from '@mui/material'
-
+import { styled } from '@mui/material'
 import { ShirtIcon, UserDataIcon } from '../assets'
+
+import { Field } from './Field'
+
+const Container = styled('div')({
+   display: 'flex',
+   alignItems: 'flex-start',
+})
+
+const Icon = styled('img')({
+   width: '21.438rem',
+   height: '21.438rem',
+   flexShrink: '0',
+   borderRadius: '0.5rem',
+   padding: '1.25rem',
+})
+
+const Image = styled('img')({
+   width: '2.5rem',
+   height: '2.5rem',
+   flexShrink: '0',
+   borderRadius: '2.5rem',
+   marginTop: '2.5rem',
+})
+
+const MainContext = styled('p')({
+   fontSize: '1.25rem',
+})
+
+const Main = styled('div')({
+   marginTop: '-1.875rem',
+})
+
+const Span = styled('span')({
+   color: '#5c5c5c',
+})
+
+const FrowContent = styled('div')({
+   margin: '3.438rem',
+   marginTop: '-2.188rem',
+})
+
+const FieldText = styled('p')({
+   marginTop: '1.25rem',
+})
+
+const Title = styled('span')({
+   color: '#3774d0',
+   margin: '7.5rem',
+})
 
 export function AdminState() {
    return (
-      <div>
-         <img src={ShirtIcon} alt="shirt" />
-         <img src={UserDataIcon} alt="user-data" />
-         <span>Аида Каримова</span>
-         <span>+996 705 86 95 44</span>
+      <Container>
+         <Icon src={ShirtIcon} alt="shirt" />
          <div>
-            <b>Рубашка</b>
-            <p>
-               Рубашка с технологией ProMotion и быстрым, плавным откликом.
-               Грандиозный апгрейд системы камер, открывающий совершенно новые
-               возможности. Исключительная прочность. A15 Bionic — самый быстрый
-               чип для iPhone. И впечатляющее время работы без подзарядки. Всё
-               это Pro.
-            </p>
+            <Image src={UserDataIcon} alt="user-data" />
+            <FrowContent>
+               <span>Аида Каримова</span>
+               <div>
+                  <Span>+996 705 86 95 44</Span>
+                  <Title>в ожидании</Title>
+               </div>
+            </FrowContent>
+            <Main>
+               <MainContext>Рубашка</MainContext>
+               <div>
+                  <FieldText>
+                     Рубашка с технологией ProMotion и быстрым, плавным
+                     откликом. Грандиозный апгрейд системы камер, открывающий
+                     совершенно новые возможности. Исключительная прочность. A15
+                     Bionic — самый быстрый чип для iPhone. И впечатляющее время
+                     работы без подзарядки. Всё это Pro.
+                  </FieldText>
+               </div>
+            </Main>
          </div>
-         <p>Категория:</p>
-         <p>Школьные</p>
-         <p>Состояние:</p>
-         <p>Б/У</p>
-         <p>Подкатегория:</p>
-         <p>Сумка</p>
-         <p>Дата добавления:</p>
-         <p>12.04.2022</p>
-         <div>
-            <Button  type="button">Удалить</Button>
-            <Button type="button">Заблокировать</Button>
-         </div>
-      </div>
+         <Field />
+      </Container>
    )
 }
---
+++ src/components/Field.jsx
@@ -0,0 +1,64 @@
+import { Button, styled } from '@mui/material'
+
+const Display = styled('div')({
+   display: 'flex',
+})
+
+const Around = styled('div')({
+   display: 'flex',
+})
+
+const FLowContent = styled('div')({
+   padding: '12.5rem',
+   marginTop: '1.25rem',
+   marginLeft: '-1.875rem',
+})
+const DefContent = styled('div')({
+   marginLeft: '-51.25rem',
+})
+const Paragraph = styled('p')({
+   padding: '1.25rem',
+   marginTop: '1.25rem',
+   color: '#5c5c5c',
+})
+const ButtonContainer = styled('div')({
+   display: 'flex',
+})
+const TextFeature = styled('p')({
+   color: '#000',
+})
+
+export function Field() {
+   return (
+      <div>
+         <FLowContent>
+            <DefContent>
+               <Display>
+                  <Paragraph>
+                     Категория: <TextFeature>Школьные</TextFeature>
+                  </Paragraph>
+                  <Paragraph>
+                     Состояние: <TextFeature>Б/У</TextFeature>
+                  </Paragraph>
+               </Display>
+               <Around>
+                  <Paragraph>
+                     Подкатегория: <TextFeature>Сумка</TextFeature>
+                  </Paragraph>
+                  <Paragraph>
+                     Дата добавления: <TextFeature>12.04.2022</TextFeature>
+                  </Paragraph>
+               </Around>
+            </DefContent>
+            <ButtonContainer>
+               <Button variant="containeds" type="button">
+                  Удалить
+               </Button>
+               <Button variant="contained" type="button">
+                  Заблокировать
+               </Button>
+            </ButtonContainer>
+         </FLowContent>
+      </div>
+   )
+}
