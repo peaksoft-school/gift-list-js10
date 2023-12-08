e78db7af25c45333bc7ae7db49b60e7a8b9b57b1
Kasym Ulanbekov <ulanbek.kasym@gmail.com>
2023-12-08 15:01:45 +0600

    fixed the toastify

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -29,7 +29,7 @@ axiosInstance.interceptors.response.use(
       if (error.response.status === 401) {
          store.dispatch(logout())
       }
-      return Promise.reject(error.response.data)
+      return Promise.reject(error)
    }
 )
 
